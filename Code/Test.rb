97f5f4e3d80f81ed406b20f16efad9d5b2ede842
godefroy29 <godefroy.29@gmail.com>
2015-01-21 18:48:14 +0100

    maj

--- Code/Test.rb
+++ Code/Test.rb
@@ -1,15 +1,19 @@
 load 'Jeu.rb'
 jeu1=Jeu.new("01_1")
 jeu1.affichageJeu
+puts
 puts "test changement tuile jouable"
 jeu1.etatSuivant(2)
 jeu1.affichageJeu
+puts
 puts "test changement tuile jouable"
 jeu1.etatSuivant(2)
 jeu1.affichageJeu
+puts
 puts "test changement tuile jouable"
 jeu1.etatSuivant(2)
 jeu1.affichageJeu
+puts
 puts "test changement tuile jouable"
 jeu1.etatSuivant(2)
 jeu1.affichageJeu
